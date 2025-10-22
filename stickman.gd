477bffa5bfa1a5dbf328bdb5faa07676cb273028
Schiggy <hillbrecht.s@gmail.com>
2025-10-22 12:57:58 +0200

    punch animation

--- stickman.gd
+++ stickman.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
-enum Anim {IDLE, RUN}
+enum Anim {IDLE, RUN, PUNCH}
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 
-@export var animation : Anim
+@export var play_animation : Anim
 
 func _ready() -> void:
-	match animation:
+	match play_animation:
 		Anim.IDLE:
 			animation_player.play("idle")
 		Anim.RUN:
 			animation_player.play("run")
+		Anim.PUNCH:
+			animation_player.play("punch")
